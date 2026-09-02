2d40fff20814a51988a7b622f7a6f843b61fc0f8
ridhima2030 <ridhima.chaturvedi.cs27@iilm.edu>
2026-09-02 11:20:07 +0530

    Implement maximal square calculation in Python

---
+++ Day 15/221. Maximal Square.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximalSquare(self, matrix):
+        rows, cols = len(matrix), len(matrix[0])
+        dp = [[0] * (cols + 1) for _ in range(rows + 1)]
+        max_side = 0
+
+        for r in range(1, rows + 1):
+            for c in range(1, cols + 1):
+                if matrix[r-1][c-1] == '1':
+                    dp[r][c] = min(dp[r-1][c], dp[r][c-1], dp[r-1][c-1]) + 1
+                    max_side = max(max_side, dp[r][c])
+
+        return max_side * max_side
